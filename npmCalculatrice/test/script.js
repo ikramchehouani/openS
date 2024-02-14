7aab9df6ac90e732e0141e755ba73bcca96fc6a0
armelsteve1 <ouetchouaarmel@yahoo.com>
2024-02-14 11:53:13 +0100

    add calculatrice

---
+++ npmCalculatrice/test/script.js
@@ -0,0 +1,17 @@
+const helloWorld = require('../index.js');
+console.log(helloWorld());
+
+const addition = require('../calco.js');
+console.log(addition(5, 3));
+
+const soustraction = require('../calco.js');
+console.log(soustraction(10, 4));
+
+const multiplication = require('../calco.js');
+console.log(multiplication(2, 6));
+
+const division = require('../calco.js');
+console.log(division(10, 2));
+
+const racineCarree = require('../calco.js');
+console.log(racineCarree(9));
